7947d4307ac4cc64be86b30d1dfbffb3fc541ed0
Valdir <valdirsb@gmail.com>
2022-02-23 00:38:30 -0300

    https

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://api.openweathermap.org/data/2.5';
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
 const api = axios.create({
  baseURL: baseUrl,
